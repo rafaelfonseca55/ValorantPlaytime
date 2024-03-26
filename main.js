0d2def4ea6ca82394031aa1d7510751f21da415a
Rafael Fonseca <62186306+rafaelfonseca55@users.noreply.github.com>
2024-03-26 02:02:46 +0000

    Fixed documentation issues

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
+const serverId = '';
+const userId = '';
+const channelId = '';
+const token = '';
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -10,7 +14,7 @@ const client = new Client({
 
 let valorantPlaytimeStart;
 
-// Read the playtime.json file and parse the JSON data
+// Reads the playtime.json file and parses the JSON data
 if (fs.existsSync('playtime.json')) {
     const data = fs.readFileSync('playtime.json', 'utf8');
     const jsonData = JSON.parse(data);
@@ -20,12 +24,12 @@ if (fs.existsSync('playtime.json')) {
 client.on('ready', async () => {
     console.log(`Bot is ready as: ${client.user.tag}`);
     setInterval(async () => {
-        const guild = client.guilds.cache.get('802529062563479583'); // SERVER ID
-        const member = await guild.members.fetch('746887218513051689'); // USER ID
-
+        const guild = client.guilds.cache.get(serverId);
+        const member = await guild.members.fetch(userId);
+// If the user has Rich Presence enabled, it checks if he's playing Valorant or not
         if(member.presence != null && member.presence.activities != null) {
             const valorantActivity = member.presence.activities.find(activity => activity.name === 'VALORANT');
-
+// If he's playing timer starts
             if (valorantActivity) {
                 if (!valorantPlaytimeStart) {
                     valorantPlaytimeStart = new Date(valorantActivity.timestamps.start);
@@ -33,7 +37,7 @@ client.on('ready', async () => {
 
                 const valorantPlaytime = Math.floor((new Date() - valorantPlaytimeStart) / 60000); // in minutes
                 const valorantPlaytimeInHours = (valorantPlaytime / 60).toFixed(2); // in hours
-                const channel = guild.channels.cache.get('1192902533119746138'); // channel ID
+                const channel = guild.channels.cache.get('channelId'); // channel ID
                 fs.writeFileSync('playtime.json', JSON.stringify({ valorantPlaytime, valorantPlaytimeStart }));
                 channel.send(`Valorant playtime: ${valorantPlaytimeInHours} hours.`);
             } else {
@@ -43,4 +47,4 @@ client.on('ready', async () => {
     }, 60000); // check every minute
 });
 
-client.login('$BOTAPI'); // BOT TOKEN
+client.login(token); // BOT TOKEN
